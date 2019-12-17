b0275477660b7effbc08e79c29f15d38c4b22ae7
yoyo <yoyo@yoyo.com>
2019-12-18 07:25:58 +0800

    add

--- Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
+++ Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
@@ -3,12 +3,16 @@ package geektime.spring.data.declarativetransactiondemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class FooServiceImpl implements FooService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private FooService fooService;
 
     @Override
     @Transactional
@@ -17,7 +21,7 @@ public class FooServiceImpl implements FooService {
     }
 
     @Override
-    @Transactional(rollbackFor = RollbackException.class)
+    @Transactional(rollbackFor = RollbackException.class,propagation = Propagation.NESTED)
     public void insertThenRollback() throws RollbackException {
         jdbcTemplate.execute("INSERT INTO FOO (BAR) VALUES ('BBB')");
         throw new RollbackException();
@@ -25,6 +29,12 @@ public class FooServiceImpl implements FooService {
 
     @Override
     public void invokeInsertThenRollback() throws RollbackException {
-        insertThenRollback();
+        fooService.insertThenRollback();
     }
+
+//
+//    @Override
+//    public void invokeInsertThenRollback() throws RollbackException {
+//        insertThenRollback();
+//    }
 }
